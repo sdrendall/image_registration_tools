beb61fe4a0e212a19fab59061d110bc5ef573666
Sam Rendall <srendall@gmail.com>
2015-05-30 16:02:58 -0400

    Began writing transform application functions

---
+++ apply_transform.cpp
@@ -0,0 +1,102 @@
+#include "apply_transform.h"
+
+using namespace std;
+
+// option parsing
+struct Arg: public option::Arg
+ {
+   static void printError(const char* msg1, const option::Option& opt, const char* msg2)
+   {
+     fprintf(stderr, "ERROR: %s", msg1);
+     fwrite(opt.name, opt.namelen, 1, stderr);
+     fprintf(stderr, "%s", msg2);
+   }
+
+   static option::ArgStatus Unknown(const option::Option& option, bool msg)
+   {
+     if (msg) printError("Unknown option '", option, "'\n");
+     return option::ARG_ILLEGAL;
+   }
+
+   static option::ArgStatus Required(const option::Option& option, bool msg)
+   {
+     if (option.arg != 0)
+       return option::ARG_OK;
+
+     if (msg) printError("Option '", option, "' requires an argument\n");
+     return option::ARG_ILLEGAL;
+   }
+
+   static option::ArgStatus Numeric(const option::Option& option, bool msg)
+   {
+     char* endptr = 0;
+     if (option.arg != 0 && strtol(option.arg, &endptr, 10)){};
+     if (endptr != option.arg && *endptr == 0)
+       return option::ARG_OK;
+
+     if (msg) printError("Option '", option, "' requires a numeric argument\n");
+     return option::ARG_ILLEGAL;
+   }
+ };
+
+
+enum optionIndex {
+    UNKNOWN,
+    HELP,
+    MOVING_IMAGE,
+    OUTPUT_PATH,
+    TRANSFORM_PATH,
+    INVERT_TRANSFORM
+};
+
+
+const option::Descriptor usage[] = {
+    {UNKNOWN, 0, "", "", Arg::Unknown, "USAGE: image_to_image_registration [options]\n\n"
+                                       "Options: "},
+    {HELP, 0, "h", "help", Arg::None, "--help, -h \tDisplay this help message and exit"},
+    {MOVING_IMAGE, 0, "m", "moving", Arg::Required, "--moving, -m path \tPath to the moving image to apply the transform to."},
+    {OUTPUT_PATH, 0, "o", "output", Arg::Required, "--output, -o path \tPath to save the transformed moving image"},
+    {TRANSFORM_PATH, 0, "t", "transform", Arg::Required, "--transform, -t path \tPath to the transform to apply"},
+    {INVERT_TRANSFORM, 0, "i", "invert", Arg::None, "--invert, -i \tInvert the given transform"},
+    {0,0,0,0,0,0}
+};
+
+
+int main(int argc, char** argv) {
+    // parse options
+    argv += (argc > 0);
+    argc -= (argc > 0);
+
+    option::Stats stats(usage, argc, argv);
+    option::Option* options = new option::Option[stats.options_max];
+    option::Option* buffer = new option::Option[stats.buffer_max];
+    option::Parser parse(usage, argc, argv, options, buffer);
+
+    if (options[HELP]) {
+        option::printUsage(cout, usage);
+        return 1;
+    }
+
+    if (!options[MOVING_IMAGE] || !options[OUTPUT_PATH] || !options[TRANSFORM_PATH]) {
+        cout << "Insufficient Arguments!!" << endl;
+        cout << "Please specify a moving image, an output path, and a transform to apply." << endl;
+        option::printUsage(cout, usage);
+        return 1;
+    }
+
+    // Read image
+    typedef itk::Image<float, 2> ImageType; 
+    ImageType::Pointer image = load_image<ImageType>(options[MOVING_IMAGE].arg);
+
+    // If not inverse, apply normal transform
+    if (!options[INVERT_TRANSFORM]) {
+        image = apply_transform<ImageType, TRANSFORM_TYPE>(image, transform);
+    }
+
+    // Else, invert transform
+        // If has method (GetInverse) use inverse
+
+        // Else, use deformation field
+
+    return 0;
+}
---
+++ apply_transform.h
@@ -0,0 +1,38 @@
+#include "itkImage.h"
+#include "itkResampleImageFilter.h"
+#include "itkBSplineInterpolateImageFunction.h"
+
+#include "optionparser.h"
+#include "image_io.h"
+
+#include <iostream>
+
+#ifndef APPLY_TRANSFORM
+#define APPLY_TRANSFORM
+
+template<typename IMAGE_TYPE, typename TRANSFORM_TYPE>
+typename IMAGE_TYPE::Pointer apply_transform(typename IMAGE_TYPE::Pointer image, typename TRANSFORM_TYPE::Pointer transform) {
+    // Applys the transform to the image using bspline interpolation
+    typedef itk::ResampleImageFilter<IMAGE_TYPE, IMAGE_TYPE> ImageResamplerType;
+    typedef itk::BSplineInterpolateImageFunction<IMAGE_TYPE, double, double> InterpolatorType;
+
+    typename ImageResamplerType::Pointer resampler = ImageResamplerType::New();
+    resampler->SetTransform(transform);
+    resampler->SetInput(image);
+
+    // Configure the resampler to apply the BSpline Transform
+    resampler->SetSize(image->GetLargestPossibleRegion().GetSize());
+    resampler->SetOutputOrigin(image->GetOrigin());
+    resampler->SetOutputSpacing(image->GetSpacing());
+    resampler->SetOutputDirection(image->GetDirection());
+    resampler->SetDefaultPixelValue(0);
+
+    // Link the interpolator to the resamplers
+    typename InterpolatorType::Pointer interpolator = InterpolatorType::New();
+    resampler->SetInterpolator(interpolator);
+
+    resampler->Update();
+    return resampler->GetOutput();
+}
+
+#endif
